33f0a52f9fef8691909763f4ed8c8dfa14168a4f
Herkles <seanpatrickzeitler@gmail.com>
2020-07-23 12:00:49 -0400

    added the rank file

--- face_recognition_brain/src/components/Rank/Rank.js
+++ face_recognition_brain/src/components/Rank/Rank.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 const Rank = () => {
   return (
-
+    <div>
+      <div className='white f3'>
+        {'Test your Current Rank is...'}
+      </div>
+      <div className='white f1'>
+        {'#5'}
+      </div>
+    </div>
   );
 }
 
