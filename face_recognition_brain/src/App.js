db3402b29665d1da7566b8dc61641d60a8dd0fae
Herkles <seanpatrickzeitler@gmail.com>
2020-07-27 16:16:45 -0400

    started working on FaceRecognition component

--- face_recognition_brain/src/App.js
+++ face_recognition_brain/src/App.js
@@ -5,6 +5,7 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
 
 
@@ -37,9 +38,12 @@ class App extends Component {
 
   onButtonSubmit = () => {
     console.log('click');
-    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    app.models.predict(
+      "a403429f2ddf4b49b307e318f00e528b", 
+      "https://samples.clarifai.com/face-det.jpg")
+    .then(
       function(response) {
-        // do something with response
+      // do something with response
       },
       function(err) {
         // there was an error
@@ -64,7 +68,7 @@ class App extends Component {
           onInputChange={this.onInputChange} 
           onButtonSubmit={this.onButtonSubmit} 
         />
-        {/* <FaceRecognition /> */}
+        <FaceRecognition />
       </div>
     );
   }
--- face_recognition_brain/src/components/FaceRecognition/FaceRecognition.js
+++ face_recognition_brain/src/components/FaceRecognition/FaceRecognition.js
@@ -1,11 +1,11 @@
-// import React from 'react';
+import React from 'react';
 
-// const FaceRecognition = () => {
-//   return (
-//     <div>
-//       <img alt='' src={'https://samples.clarifai.com/face-det.jpg'}  />
-//     </div>
-//   );
-// }
+const FaceRecognition = () => {
+  return (
+    <div>
+      <img alt='' src={'https://samples.clarifai.com/face-det.jpg'}  />
+    </div>
+  );
+}
 
-// export default FaceRecognition;
+export default FaceRecognition;
