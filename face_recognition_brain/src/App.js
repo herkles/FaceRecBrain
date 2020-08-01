77a30c9f08a064d8481a481604cb746e610fe82c
Herkles <seanpatrickzeitler@gmail.com>
2020-07-31 20:24:55 -0400

    implemented route change for signing in and signing out

--- face_recognition_brain/src/App.js
+++ face_recognition_brain/src/App.js
@@ -7,6 +7,7 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import './App.css';
 
 
@@ -33,6 +34,7 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
+      isSignedIn: false,
     }
   }
 
@@ -69,6 +71,11 @@ class App extends Component {
   }
 
   onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
     this.setState({route: route})
   }
 
@@ -79,10 +86,9 @@ class App extends Component {
         <Particles className='particles'
           params={particlesOptions}
         />
-        <Navigation onRouteChange={this.onRouteChange } />
-        { this.state.route === 'signin' 
-          ? <Signin onRouteChange={this.onRouteChange} />       
-          : <div> 
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange } />
+        { this.state.route === 'home' 
+          ? <div> 
               <Logo />
               <Rank />
               <ImageLinkForm 
@@ -91,6 +97,11 @@ class App extends Component {
               />
               <FaceRecognition box={this.state.box}  imageUrl={this.state.imageUrl}   />         
             </div>
+          : ( 
+              this.state.route === 'signin'
+              ? <Signin onRouteChange={this.onRouteChange} />
+              : <Register onRouteChange={this.onRouteChange} />
+            )
         }
       </div>
     );
--- face_recognition_brain/src/components/Register/Register.js
+++ face_recognition_brain/src/components/Register/Register.js
@@ -6,7 +6,7 @@ const Register = ({ onRouteChange }) => {
       <main className="pa4 black-80">
         <form className="measure">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-            <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+            <legend className="f1 fw6 ph0 mh0">Register</legend>
             <div className="mt3">
               <label className="db fw6 lh-copy f6" for="email-address">Email</label>
               <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
@@ -19,9 +19,9 @@ const Register = ({ onRouteChange }) => {
           <div className="">
             <input 
               onClick={() => onRouteChange('home')}
-              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib pointer" 
               type="submit" 
-              value="Sign in" 
+              value="Register" 
             />
           </div>
         </form>
