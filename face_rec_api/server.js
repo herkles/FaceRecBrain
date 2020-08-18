e86caf391bf3153d6b5fc1eb0ae8f7fa4ba7fdd2
Herkles <seanpatrickzeitler@gmail.com>
2020-08-18 13:57:27 -0400

    began implentation of the register fuctionality

--- face_rec_api/server.js
+++ face_rec_api/server.js
@@ -26,7 +26,7 @@ const database = {
 }
 
 app.get('/', (req, res) => {
-  res.send('this is working');
+  res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
@@ -37,6 +37,20 @@ app.post('/signin', (req, res) => {
       res.status(400).json('Error Logging In')
     }
 })
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: '125',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  })
+  res.json(database.users[database.users.length-1]);
+})
+
 
 app.listen(3000, ()=> {
   console.log('app is running on port 3000');
