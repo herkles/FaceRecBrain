7a7b647aa695955fea11a416a8d737d6f6a15709
Herkles <seanpatrickzeitler@gmail.com>
2020-09-02 14:27:13 -0400

    implemented profile

--- face_rec_api/server.js
+++ face_rec_api/server.js
@@ -5,8 +5,6 @@ const cors = require('cors');
 const app = express();
 const knex = require('knex')
 
-
-
 const db  = knex({
   client: 'pg',
   connection: {
@@ -16,9 +14,6 @@ const db  = knex({
     database : 'smart-brain'
   }
 });
-
-
-
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -66,26 +61,30 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
-  db('users').insert({
-    email: email,
-    name: name,
-    joined: new Date()
-  }).then(console.log)
-  res.json(database.users[database.users.length-1]);
+  db('users')
+    .returning('*')
+    .insert({
+      email: email,
+      name: name,
+      joined: new Date()
+    })
+      .then(user => {
+        res.json(user[0]);
+    })
+    .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    } 
+  db.select('*').from('users').where({id})  
+  .then(user => {
+    if (user.length) {
+      res.json(user[0])
+    } else {
+      res.status(400).json('Not Found')
+    }
   })
-  if (!found) {
-    res.status(400).json('not found');
-  }
+    .catch(err => res.status(400).json('Not Found'))
 })
 
 app.put('/image', (req, res) => {
