fa58f5f95aa7731a925250e955e162ee5e208646
Herkles <seanpatrickzeitler@gmail.com>
2020-09-04 15:16:36 -0400

    the backend signin now connects to the database

--- face_rec_api/server.js
+++ face_rec_api/server.js
@@ -18,45 +18,27 @@ const db  = knex({
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      email: 'john@gmail.com',
-      password: 'test',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '124',
-      name: 'Sally',
-      email: 'sally@gmail.com',
-      password: 'bananas',
-      entries: 0,
-      joined: new Date()
-    },
-  ],
-  login: [
-    {
-      id: '987',
-      has: '',
-      email: 'john@gmail.com'
-    }
-  ]
-}
-
 app.get('/', (req, res) => {
   res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
-  if (req.body.email === database.users[0].email &&
-      req.body.password === database.users[0].password) {
-    res.json(database.users[0]);
-    } else {
-      res.status(400).json('Error Logging In')
-    }
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+     const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+     if (isValid) {
+      return db.select('*').from('users')
+        .where('email', '=', req.body.email) 
+        .then(user => {
+          res.json(user[0])
+        })
+        .catch(err => res.status(400).json('Unable to get User'))
+     } else {
+      res.status(400).json('Wrong Credentials')
+     }
+    })
+    .catch(err => res.status(400).json('wrong Credentials'))
 })
 
 app.post('/register', (req, res) => {
